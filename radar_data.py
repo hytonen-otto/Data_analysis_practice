4c8900bd244f5ba8afb13b3297025d53f7246b60
hytonen-otto <otto.hytonen@gmail.com>
2024-05-07 18:21:38 +0300

    Changed the name of the python file

---
+++ radar_data.py
@@ -0,0 +1,8 @@
+from fmiopendata.wfs import download_stored_query
+
+#Joko nyt on oikea nimi vakiona?
+
+composites = download_stored_query("fmi::radar::composite::dbz")
+
+# %%
+#Ajettavia soluja tehdään näin
