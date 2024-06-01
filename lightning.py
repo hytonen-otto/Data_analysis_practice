c2533dce9a76ffd95f71e1247a3ca6446bf4e7e0
hytonen-otto <otto.hytonen@gmail.com>
2024-06-01 18:58:21 +0300

    Final changes to lightning.py

--- lightning.py
+++ lightning.py
@@ -11,22 +11,21 @@ import datetime as dt
 
 latN = 60.5
 latS = 60.15
-lonW = 24.5 
+lonW = 24.5
 lonE = 25.4
+zoom_level = 10
 
-
-
+'''
 #Time window for the data 
 start_time = '2024-06-01T11:15:00Z'
 end_time = '2024-06-01T11:20:00Z'
-
 '''
+
 #Retrieving the latest 15min of data from a bounding box
-end_time = dt.datetime.now()
+end_time = dt.datetime.now(dt.timezone.utc).replace(tzinfo=None)
 start_time = end_time - dt.timedelta(minutes=15)
 start_time = start_time.isoformat(timespec="seconds") + "Z" # Convert times to properly formatted strings
 end_time = end_time.isoformat(timespec="seconds") + "Z" # -> 2020-07-07T12:00:00Z
-'''
 
 
 #Fetching the data
@@ -40,23 +39,12 @@ try:
 except:
     exit() #Ending code run if not lightning in the area
     
-
-'''
-lightning1.latitudes  # Latitude of the lightning event [° North]
-lightning1.longitudes  # Longitude of the lightning event [° East]
-lightning1.times  # Time of the lightning event [datetime]
-lightning1.cloud_indicator  # Indicator for cloud flashes (1 == cloud lightning)
-lightning1.multiplicity  # Multiplicity of the lightning event
-lightning1.peak_current  # Maximum current of the lightning event [kA]
-lightning1.ellipse_major  # Location accuracy of the lightning event [km]
-'''
-
 request = cimgt.OSM() #Requesting open street map
 
 #Drawing the plot
 fig1 = plt.figure(figsize=(9,5),layout="constrained")
 ax = plt.subplot(projection=request.crs) #Specifying the projection
-ax.add_image(request,11,interpolation='none') #Adding the map
+ax.add_image(request,zoom_level,interpolation='none') #Adding the map
 ax.set_extent([lonW, lonE, latS, latN]) #Limiting the map area (Crashs without this)
 ax.gridlines(draw_labels=True, zorder=5) #Drawing grid lines
 
@@ -67,6 +55,6 @@ for i in range(len(lightning1.times)):
     ax.add_geometries((geom,), crs=ccrs.PlateCarree(), facecolor='none', edgecolor='red', linewidth=1) 
     #Adding circles to map, NOTICE the projection transformation (the data is in PlateCarree format)
 
-plt.title(f'Observed lightning {lightning1.times[0].date()}, {lightning1.times[0].time()}-{lightning1.times[-1].time()}')
+plt.title(f'Observed lightning {lightning1.times[0].date()}, {start_time[11:-1]}-{end_time[11:-1]} UTC')
 plt.show()
 #fig1.savefig('kuva1.png',dpi=500)
